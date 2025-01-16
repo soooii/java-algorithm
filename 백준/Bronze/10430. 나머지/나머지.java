8621da716de40e78a03b82d74643e6e286b8d9bf
songheeAn <songhee9327@gmail.com>
2025-01-16 11:18:34 +0900

    [Bronze V] Title: 나머지, Time: 172 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -6,11 +6,17 @@ public class Main{
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
+        
         int answer1 = (a+b)%c;
         int answer2 = ((a%c)+(b%c))%c;
         int answer3 = (a*b)%c;
         int answer4 = ((a%c)*(b%c))%c;
         
+        /*
+        모듈러 연산 증명
+        answer1 = answer2
+        answer3 = answer4
+        */
         System.out.println(answer1);
         System.out.println(answer2);
         System.out.println(answer3);
