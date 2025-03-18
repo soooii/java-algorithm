46aa555b75bc069cc4f824e7cb12ac2c0dd765a2
songheeAn <songhee9327@gmail.com>
2025-03-18 13:35:33 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,32 +1,18 @@
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String f = fac(n).toString();
-        String[] ff = f.split("");
         int count = 0;
-        for(int i=ff.length-1;i>=0;i--){
-            if(ff[i].equals("0")){
-                count++;
-            }else{
-                System.out.println(count);
-                break;
+
+        for(int i=1; i<=n; i++){
+            int nn =i;
+            while(nn%5 == 0){
+                count ++;
+                nn = nn/5;
             }
         }
-
-
-    }
-
-    // int, long 타입 반환 시 오버플로우
-    // 더 큰 정수 지원하는 BigInteger 타입 반환
-    public static BigInteger fac(int n){
-        BigInteger answer = BigInteger.ONE;
-        for(int i=0;i<n;i++){
-            answer = answer.multiply(BigInteger.valueOf(n-i));
-        }
-        return answer;
+        System.out.println(count);
     }
 }
