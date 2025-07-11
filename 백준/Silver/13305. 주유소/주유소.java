acbc0ce256a6eb88e93cda336f8129aabae7acf6
songheeAn <songhee9327@gmail.com>
2025-07-12 04:02:07 +0900

    [Silver III] Title: 주유소, Time: 300 ms, Memory: 33204 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 왼 -> 오
+        // 도시들 사이 거리 존재
+        // 1km당 1리터 기름, 도시마다 주유소 존재
+        // 리터당 기름값 각 주유소마다 다름
+
+        int n = Integer.parseInt(br.readLine()); // 도시 개수
+        String[] len = br.readLine().split(" "); // 도시 간 거리
+        String[] price = br.readLine().split(" "); // 리터당 가격
+
+        int minPrice = Integer.parseInt(price[0]);
+        int total=0;
+        // 맨 왼쪽 도시는 무조건 기름 넣어야함..
+        for(int i = 0; i < n-1; i++){
+            if(Integer.parseInt(price[i])<minPrice){
+                minPrice = Integer.parseInt(price[i]);
+            }
+            
+            total += minPrice*Integer.parseInt(len[i]);
+        }
+
+        System.out.println(total);
+        
+    }
+}
