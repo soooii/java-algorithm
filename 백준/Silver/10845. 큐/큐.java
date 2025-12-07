7207395e5be8aa5768eb9f45080542c2074f706b
AN SONGHEE <songhee9327@gmail.com>
2025-12-08 03:25:07 +0900

    [Silver IV] Title: 큐, Time: 156 ms, Memory: 19400 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,58 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.io.*; // BufferedReader, IOException, InputStreamReader
+import java.util.*; // StringTokenizer, Queue, LinkedList
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < n; i++){
-            String s = br.readLine();
-            String[] ss = s.split(" ");
-            if(ss[0].equals("push")){
-                list.add(Integer.parseInt(ss[1]));
-            }
-            if(ss[0].equals("pop")){
-                if(list.isEmpty()){
-                    sb.append(-1+"\n");
-                }else{
-                    sb.append(list.get(0) + "\n");
-                    list.remove(0);
-                }
-            }
-            if(ss[0].equals("size")){
-                sb.append(list.size() + "\n");
-            }
-            if(ss[0].equals("empty")){
-                if(list.isEmpty()){
-                    sb.append(1 + "\n");
-                }else{
-                    sb.append(0 + "\n");
-                }
-            }
-            if(ss[0].equals("front")){
-                if(list.isEmpty()){
-                    sb.append(-1+"\n");
-                }else{
-                    sb.append(list.get(0) + "\n");
-                }
-            }
-            if(ss[0].equals("back")){
-                if(list.isEmpty()){
-                    sb.append(-1+"\n");
-                }else{
-                    sb.append(list.get(list.size()-1) + "\n");
-                }
-            }
-
-        }
-
-        bw.write(sb.toString());
-
-        bw.flush();
-        bw.close();
-
-    }
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+        
+		StringBuilder sb = new StringBuilder();
+		Queue<Integer> que = new LinkedList<Integer>();
+		int last = 0;
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+            String S = st.nextToken();
+			
+			switch(S) {
+			case "push" :
+				last = Integer.parseInt(st.nextToken());
+				que.offer(last);
+				break;
+			case "pop" :
+				if(que.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(que.poll()).append("\n");
+				break;
+			case "size" :
+				sb.append(que.size()).append("\n");
+				break;
+			case "empty" :
+				if(que.isEmpty()) sb.append(1).append("\n");
+				else sb.append(0).append("\n");
+				break;
+			case "front" :
+				if(que.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(que.peek()).append("\n");
+				break;
+			case "back" :
+				if(que.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(last).append("\n");
+				break;
+			}
+		}
+		System.out.println(sb);
+	}
+	
 }
