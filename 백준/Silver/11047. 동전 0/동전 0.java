c5517c9bda8db945b05eb2b46367563d122b0592
AN SONGHEE <songhee9327@gmail.com>
2026-04-09 18:42:07 +0900

    [Silver IV] Title: 동전 0, Time: 2312 ms, Memory: 400832 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,34 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String s = br.readLine();
-        StringTokenizer st = new StringTokenizer(s);
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken()); //목표
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i=0;i<N;i++){
-            stack.push(Integer.parseInt(br.readLine()));
-        }
-        
-        int count =0;
-        while(K!=0){
-          if(stack.peek()<=K){
-            count += K/stack.peek();
-            K = K%stack.peek();
-            stack.pop();
-            }else{
-            stack.pop();
-            }
-        }
-        
-        System.out.println(count);
-       
-        
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N=Integer.parseInt(st.nextToken());
+		int K=Integer.parseInt(st.nextToken());
+
+		int[] A = new int[N+1];
+		for(int i=1;i<=N;i++){
+			A[i] = Integer.parseInt(br.readLine());
+		}
+
+		int[] dp = new int[K+1];
+		for(int i=0;i<=K;i++){
+			dp[i]=K+1;
+		}
+
+		dp[0]=0;
+
+		for(int i=0;i<=K;i++){
+			for(int j=1;j<=N;j++){
+				if(i>=A[j]){
+					dp[i]=Math.min(dp[i],dp[i-A[j]]+1);
+				}
+			}
+		}
+
+		if (dp[K] == K+1) {
+			System.out.println(-1);
+		} else {
+			System.out.println(dp[K]);
+		}
+	}
 }
