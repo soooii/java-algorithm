eaf986a959d109a25ed9cce7b4b17f59edbc160c
AN SONGHEE <songhee9327@gmail.com>
2026-07-29 19:46:17 +0900

    [level 2] Title: 숫자의 표현, Time: 0.17 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        int cnt=0;
+        
+        // 정수론
+        for(int i=1;i<=n;i++){
+          if(n%i == 0 && i%2 != 0){
+              cnt++;
+          }
+        }
+        return cnt;
     }
 }
