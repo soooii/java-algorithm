63e3ea4de2cd542db32457e3ca77d69874751da4
AN SONGHEE <songhee9327@gmail.com>
2026-07-29 20:00:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.25 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 57.8 MB, 시간: 5.12 ms
+메모리: 57.6 MB, 시간: 5.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 29일 18:45:10
+2026년 07월 29일 20:00:31
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,65 +1,57 @@
-import java.io.*;
 import java.util.*;
-
 class Solution {
-    
-    static int[] dx={0,0,1,-1};
-    static int[] dy={-1,1,0,0};
+    static int[] dx={-1,1,0,0};
+    static int[] dy={0,0,-1,1};
     static boolean[][] visited;
-    static int[][] map;
-    int n,m;
+    static int n,m;
     
     static class Node{
-        int dist;
-        int x;
-        int y;
-        Node(int x, int y, int dist){
+        int x,y, dist;
+        
+        public Node(int x, int y, int dist){
             this.x=x;
             this.y=y;
             this.dist=dist;
         }
     }
-
     public int solution(int[][] maps) {
-        int row = maps.length;
-        int col = maps[0].length;
-        visited = new boolean[row][col];
-        map = maps;
-        n = row-1;
-        m = col-1;
-        return bfs(0,0);
+        
+        n = maps.length;
+        m = maps[0].length;
+        visited = new boolean[n][m];
+        
+        int answer = bfs(0,0,maps);
+        
+        return answer;
         
     }
-
-    public int bfs(int x, int y) {
+    
+    static int bfs(int x, int y, int[][] maps){
         Queue<Node> q = new LinkedList<>();
+        
         q.offer(new Node(x,y,1));
         visited[x][y]=true;
         
         while(!q.isEmpty()){
-            Node cur = q.poll();
+            Node current = q.poll();
             
-            if(cur.x==n && cur.y==m){
-                return cur.dist;
+            if(current.x==n-1 && current.y==m-1){
+                return current.dist;
             }
             
             for(int i=0;i<4;i++){
-                int nx = cur.x+dx[i];
-                int ny = cur.y+dy[i];
+                int nx=current.x+dx[i];
+                int ny=current.y+dy[i];
                 
-                if(nx<0 || nx>n || ny<0 || ny>m ) continue;
-                
-                if(map[nx][ny]==0) continue;
-                
-                if(!visited[nx][ny]){
-                    visited[nx][ny]=true;
-                    q.offer(new Node(nx,ny,cur.dist+1));
+                if(nx>=0 && ny>=0 && nx<n && ny<m){
+                    if(maps[nx][ny]==1 && !visited[nx][ny]){
+                        visited[nx][ny]=true;
+                        q.offer(new Node(nx,ny,current.dist+1));
+                    }
                 }
-                
             }
         }
         
         return -1;
     }
 }
-
